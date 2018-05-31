74df6512054512e9f2271e0d5da643086c46e385
Taylor Davidson <taylordavidson@ymail.com>
2018-05-31 00:33:51 -0700

    edited comments

--- replaceAllE.js
+++ replaceAllE.js
@@ -8,10 +8,21 @@ function replaceLetter(str) {
     return str.replace(/e{2,}/g, 'e')
 }
 
-// recusrively w/o regex
+// recursively w/o regex
 function replaceNoRegex(str) {
     if ((pos = str.indexOf('ee')) !== -1) {
         str = str.slice(0, pos) + str.slice(pos + 1);
+        str = replaceNoRegex(str);
+    }
+    return str
+}
+
+// second func modified to handle multiple e's greater than 2 consecutively 
+function replaceNoRegex(str) {
+    if ((pos = str.indexOf('ee')) !== -1) {
+        let end = pos + 1;
+        while (str[end] === 'e') { end++ }
+        str = str.slice(0, pos) + str.slice(end - 1);
         str = replaceNoRegex(str);
     }
     return str
